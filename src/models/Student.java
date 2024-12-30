7f172dce298ea9d3df35e9d6c068a30b45392e33
mm <242398@astanait.edu.kz>
2024-12-30 13:02:24 +0500

    added Student

---
+++ src/models/Student.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Student extends Person{
+    public Student(String name, String surname, int age, boolean gender) {
+        super(name, surname, age, gender);
+    }
+}
